951f04f918fb27321143b0580cc8be1b8c09df82
Chung <dev.chpxu@outlook.com>
2024-10-29 16:18:51 +0000

    Add jet number selection

--- src/main.py
+++ src/main.py
@@ -4,33 +4,34 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 
 # ======= global matplotlib params =====
-plt.rcParams['text.usetex'] = True  # Use LaTeX for rendering text
+plt.rcParams['text.usetex'] = False  # Use LaTeX for rendering text
 plt.rcParams['font.size'] = 12      # Set default font size (optional)
 
 pile_path = "../data/1-initial/pileup.csv"
 tt_path = "../data/1-initial/ttbar.csv"
 # jetnumber , pdgid , charge , px , py , pz
 pile_up = np.genfromtxt(pile_path, delimiter=",", encoding="utf-8", skip_header=1, max_rows=10)
-tt = np.genfromtxt(tt_path, delimiter=",", encoding="utf-8", skip_header=1, max_rows=10)
-# tt_bar = np.loadtxt(tt_path)
+tt = np.genfromtxt(tt_path, delimiter=",", encoding="utf-8", skip_header=1)
 
-def p_magnitude(p_x, p_y, p_z):
+max_tt_num = np.max(tt[:,0])
+max_pile_num = np.max(pile_up[:,0])
+# tt_bar = np.loadtxt(tt_path)
+def select_jet(data, num):
   """
-  p_x, p_y, p_z are 1D NumPy arrays where each element consists of a particle's component of momentum
+  Select data with jets #num from data file
+  """
+  if max_tt_num <= num:
+    print(f"Requested jet {num} is not in data. Max jet number is {max_tt_num}")
+    sys.exit(1)
+  return data[data[:,0] == num]
+
+def p_magnitude(p):
+  """
+  p is a 2D NumPy array where each element is a particle's 3-momentum
 
   This function simply calculates and returns the magnitude of the momenta of each particle and returns it as a 1D NumPy array
   """
-  if (np.shape(p_x) != np.shape(p_y)):
-    print("Error: p_x shape not equal to p_y shape")
-    sys.exit(1)
-  if ( np.shape(p_y) != np.shape(p_z)):
-    print("Error: p_y shape not equal to p_z shape")
-    sys.exit(1)
-  if ( np.shape(p_x) != np.shape(p_z)):
-    print("Error: p_x shape not equal to p_z shape")
-    sys.exit(1)
-
-  return np.sqrt(p_x * p_x + p_y*p_y + p_z*p_z)
+  return np.linalg.norm(p, axis=1)
 
 def pseudorapidity(p_mag, p_z):
   """
@@ -56,19 +57,25 @@ def to_phi(p_x, p_y):
     sys.exit(1)
   return np.arctan(p_y / p_x)
 
-tt_momenta = tt[:,3:]
-tt_pmag = p_magnitude(tt_momenta[:,0], tt_momenta[:,1], tt_momenta[:,2])
-tt_pz = tt[:,5]
+data = select_jet(tt, 70000)
+print(data)
+tt_momenta = data[:,3:]
+# print(tt_momenta)
+tt_pmag = p_magnitude(tt_momenta)
+print(tt_pmag)
+tt_pz = data[:,5]
+# print(tt_pz)
 tt_eta = pseudorapidity(tt_pmag, tt_pz)
 tt_phi = to_phi(tt_momenta[:,0], tt_momenta[:,1])
 fig, ax = plt.subplots(figsize=(4,4))
-ax.scatter(tt_eta, tt_phi, color='blue', marker='o')
+
+ax.scatter(tt_eta, tt_phi, color='blue', marker='o', s=0.1)
 
 # Add titles and labels
 plt.title("Scatter Plot of $\phi$ vs $\eta$")
 ax.set_xlabel("$\eta$")
-ax.set_ylabel("$\phi$")
-plt.savefig("eta_phi", format="png", dpi=100)
+ax.set_ylabel("$\\phi$")
+plt.savefig("eta_phi.png", format="png", dpi=100)
 # plt.close()
 # print("done")
 sys.exit(0)
