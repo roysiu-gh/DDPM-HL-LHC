dc8f8c311c5e3ce9882fb88e5b6db273c457a28b
Roy Siu <roysoup@outlook.com>
2024-10-29 18:08:32 +0000

    Fix indentation

--- src/main.py
+++ src/main.py
@@ -21,42 +21,42 @@ max_tt_num = np.max(tt[:,0])
 max_pile_num = np.max(pile_up[:,0])
 # tt_bar = np.loadtxt(tt_path)
 def select_jet(data, num):
-  """
-  Select data with jets #num from data file
-  """
-  if max_tt_num <= num:
-    raise ValueError(f"Requested jet {num} is not in data. Max jet number is {max_tt_num}")
-  return data[data[:,0] == num]
+    """
+    Select data with jets #num from data file
+    """
+    if max_tt_num <= num:
+        raise ValueError(f"Requested jet {num} is not in data. Max jet number is {max_tt_num}")
+    return data[data[:,0] == num]
 
 def p_magnitude(p):
-  """
-  p is a 2D NumPy array where each element is a particle's 3-momentum
+    """
+    p is a 2D NumPy array where each element is a particle's 3-momentum
 
-  This function simply calculates and returns the magnitude of the momenta of each particle and returns it as a 1D NumPy array
-  """
-  return np.linalg.norm(p, axis=1)
+    This function simply calculates and returns the magnitude of the momenta of each particle and returns it as a 1D NumPy array
+    """
+    return np.linalg.norm(p, axis=1)
 
 def pseudorapidity(p_mag, p_z):
-  """
+    """
     p_mag is a 1D NumPy array where each element is the magnitude of momentum for a particle
     p_z is the z-component of momentum of a particle (the component along the beam-axis)
 
     this function calculates the pseudorapidity and returns it as a 1D NumPy array
     https://en.wikipedia.org/wiki/Pseudorapidity
-  """
-  if ( np.shape(p_mag) != np.shape(p_z)):
-    raise ValueError("Error: p_mag shape not equal to p_z shape")
-  return np.arctanh(p_z/p_mag)
+    """
+    if ( np.shape(p_mag) != np.shape(p_z)):
+        raise ValueError("Error: p_mag shape not equal to p_z shape")
+    return np.arctanh(p_z/p_mag)
 
 def to_phi(p_x, p_y):
-  """
-  p_x, p_y are 1D NumPy arrays of x and y momenta respectively, where each element corresponds to a particle.
+    """
+    p_x, p_y are 1D NumPy arrays of x and y momenta respectively, where each element corresponds to a particle.
 
-  This function finds the angle phi (radians) from the 2 components of transverse momentum p_x,  p_y using arctan(p_y/p_x)
-  """
-  if (np.shape(p_x) != np.shape(p_y)):
-    raise ValueError("Error: p_x shape not equal to p_y shape")
-  return np.arctan2(p_y, p_x)
+    This function finds the angle phi (radians) from the 2 components of transverse momentum p_x,  p_y using arctan(p_y/p_x)
+    """
+    if (np.shape(p_x) != np.shape(p_y)):
+        raise ValueError("Error: p_x shape not equal to p_y shape")
+    return np.arctan2(p_y, p_x)
 
 jet_no = 0
 data = select_jet(tt, jet_no)
@@ -80,14 +80,14 @@ colour_mapping = {pid: cmap(i) for i, pid in enumerate(unique_pdgid)}
 colours = [colour_mapping[pid] for pid in pdgid_values]
 
 pdg_dict = {
-  -211: r"$\pi^-$ (Pion)",
-  -321: r"$K^-$ (Kaon)",
-  0: r"$\gamma$ (Photon)",
-  130: r"$K^0_S$ (K-short)",
-  211: r"$\pi^+$ (Pion)",
-  22: r"$\gamma$ (Photon)",
-  321: r"$K^+$ (Kaon)",
-  11: r"$e^-$ (Electron)",
+    -211: r"$\pi^-$ (Pion)",
+    -321: r"$K^-$ (Kaon)",
+    0: r"$\gamma$ (Photon)",
+    130: r"$K^0_S$ (K-short)",
+    211: r"$\pi^+$ (Pion)",
+    22: r"$\gamma$ (Photon)",
+    321: r"$K^+$ (Kaon)",
+    11: r"$e^-$ (Electron)",
 }
 
 # Plotting
