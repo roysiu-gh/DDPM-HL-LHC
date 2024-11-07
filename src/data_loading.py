0a17f925c869d784d25e40a65f9829340465ca1d
Chung <dev.chpxu@outlook.com>
2024-11-07 18:58:15 +0000

    Make select jet/event ID function general

--- src/data_loading.py
+++ src/data_loading.py
@@ -1,16 +1,19 @@
 # Package imports
 import numpy as np
 
-def select_jet(data, num, max_data_rows=10_000):
-    """Select data with jets #num from data file"""
+def select_event(data, num, max_data_rows=10_000):
+    """Select data with IDs num from data file. Assumes IDs are in the first (index = 0) column."""
     if (max_data_rows != None) and (max_data_rows <= num):
         raise ValueError(
             f"Requested jet {num} is not in data. Max jet number is {max_data_rows}"
         )
     return data[data[:, 0] == num]
-
-def random_rows_from_csv(data, num_rows):
-    num_rows = min(num_rows, data.shape[0])
-    random_indices = np.random.choice(data.shape[0], num_rows, replace=False)
-    random_rows = data[random_indices]
-    return random_rows
+# def extract_pile_ups(data, event_id):
+#     """
+#     Given num
+#     """
+#     num_rows = min(num_rows, data.shape[0])
+#     random_indices = np.random.choice(data.shape[0], num_rows, replace=False)
+#     event_ids = np.unique(data[random_indices][:,0])
+#     #  = data[random_indices]
+#     return data[data[:,0] == event_ids]
