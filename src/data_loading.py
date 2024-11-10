49c23e6b1cd650c1efba52eb724a8533c6ba149b
Chung <dev.chpxu@outlook.com>
2024-11-10 20:30:14 +0000

    Add function to write LID, NID

--- src/data_loading.py
+++ src/data_loading.py
@@ -1,19 +1,17 @@
 # Package imports
 import numpy as np
 
-def select_event(data, num, max_data_rows=10_000):
+def select_event(data, num, filter=False, max_data_rows=10_000):
     """Select data with IDs num from data file. Assumes IDs are in the first (index = 0) column."""
-    if (max_data_rows != None) and (max_data_rows <= num):
+    if filter == False and (max_data_rows != None) and (max_data_rows <= num):
         raise ValueError(
-            f"Requested jet {num} is not in data. Max jet number is {max_data_rows}"
+            f"Requested jet {num} is not in data. Max rows is {max_data_rows} and {num} was not found."
         )
+    # Find unique indices
+    indices = np.unique(data[:,0])
+    if (not np.isin(num, indices)):
+        print("Warn: index was not found in data file. Returning array with ID -1 (at 0th index) of same shape as 1 sample in data.")
+        zero_arr = np.zeros_like(data[0])
+        zero_arr[0] = -1
+        return zero_arr
     return data[data[:, 0] == num]
-# def extract_pile_ups(data, event_id):
-#     """
-#     Given num
-#     """
-#     num_rows = min(num_rows, data.shape[0])
-#     random_indices = np.random.choice(data.shape[0], num_rows, replace=False)
-#     event_ids = np.unique(data[random_indices][:,0])
-#     #  = data[random_indices]
-#     return data[data[:,0] == event_ids]
