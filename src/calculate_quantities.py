roysiu-gh/DDPM-HL-LHC

commit 7ef3038bea6cd04ac6a40746360ded3e8e60368d

Refactor contraction2 and four_momentum_massless to use separate parameters.

src/calculate_quantities.py
@@ -40,20 +40,35 @@ def calculate_four_momentum_massless(jet_ids, px, py, pz):
     max_jet_id = int( np.max(jet_ids) )
     total_four_momenta = np.array([np.zeros(4) for _ in range(max_jet_id + 1)])
 
+    jet_ene = np.zeros_like(jet_ids)
+    jet_px = np.zeros_like(jet_ids)
+    jet_py = np.zeros_like(jet_ids)
+    jet_pz = np.zeros_like(jet_ids)
+
     # Calculate total 4mmtm for each jet
     for jet_id, px_val, py_val, pz_val in zip(jet_ids, px, py, pz):
         energy = np.linalg.norm([px_val, py_val, pz_val])
         four_mmtm = np.array([energy, px_val, py_val, pz_val])
         total_four_momenta[int(jet_id)] += four_mmtm
 
-    return total_four_momenta
+        jet_ene[int(jet_id)] += energy
+        jet_px[int(jet_id)] += px_val
+        jet_py[int(jet_id)] += py_val
+        jet_pz[int(jet_id)] += pz_val
+
+
+    return jet_ene, jet_px, jet_py, jet_pz
 
 def contraction(vec):
-    """Calculate the contractions pf an array of 4vecs."""
+    """Calculate the contractions of an array of 4vecs."""
     time_like_0 = vec[:, 0]
     space_like_1 = vec[:, 1]
     space_like_2 = vec[:, 2]
     space_like_3 = vec[:, 3]
+    return time_like_0**2 - (space_like_1**2 + space_like_2**2 + space_like_3**2)
+
+def contraction2(time_like_0, space_like_1, space_like_2, space_like_3):
+    """Calculate the contractions."""
     return time_like_0**2 - (space_like_1**2 + space_like_2**2 + space_like_3**2)
 
 def COM_eta_phi(p):

src/main_one_time.py
@@ -39,8 +39,7 @@ p_mag = p_magnitude(px, py, pz)
 eta = pseudorapidity(p_mag, pz)
 p_T = np.sqrt(px**2 + py**2)
 # Stats for jets
-jet_four_momenta = calculate_four_momentum_massless(jet_ids, px, py, pz)
-jet_p2 = contraction(jet_four_momenta)
+jet_p2 = contraction(calculate_four_momentum_massless(jet_ids, px, py, pz))
 jet_mass = np.sqrt(jet_p2)
 
 # Kinda fun to print

src/main.py
@@ -102,8 +102,7 @@ def quantity_diff(jet_ids, jet_px, jet_py, jet_pz, pu_px, pu_py, pu_pz):
     # Case insensitivity
     # q = q.lower()
     # Calculate q_0^jet quantities. Here, since we are doing over a jet, we sum the 4-momenta and then do calcs
-    jet_four_momenta = calculate_four_momentum_massless(jet_ids, jet_px, jet_py, jet_pz)
-    jet_p2 = contraction(jet_four_momenta)
+    jet_p2 = contraction(calculate_four_momentum_massless(jet_ids, jet_px, jet_py, jet_pz))
     jet_mass = np.sum(np.sqrt(jet_p2))
     jet_pt = np.sum(np.sqrt(jet_px*jet_px + jet_py*jet_py))
     jet_energy = jet_four_momenta[0] # p^\nu = (E,px,py,pz) in natural units

commit fe901fdf44a97ffb8449f2d46c60167b32b6aaa2

Fix wrong jet_id range used in calculate_four_momentum_massless introduced in commit  7ef3038 ("Refactor contraction2 and four_momentum_massless...").

src/calculate_quantities.py
@@ -45,20 +45,17 @@ def calculate_four_momentum_massless(jet_ids, px, py, pz):
         raise ValueError(f"All input arrays must have same length. Got {len(jet_ids)}, {len(px)}, {len(py)}, {len(pz)}.")
     
     max_jet_id = int( np.max(jet_ids) )
-    total_four_momenta = np.array([np.zeros(4) for _ in range(max_jet_id + 1)])
 
-    jet_ene = np.zeros_like(jet_ids)
-    jet_px = np.zeros_like(jet_ids)
-    jet_py = np.zeros_like(jet_ids)
-    jet_pz = np.zeros_like(jet_ids)
+    jet_ene = np.zeros(max_jet_id + 1)
+    jet_px = np.zeros(max_jet_id + 1)
+    jet_py = np.zeros(max_jet_id + 1)
+    jet_pz = np.zeros(max_jet_id + 1)
 
     # Calculate total 4mmtm for each jet
     for jet_id, px_val, py_val, pz_val in zip(jet_ids, px, py, pz):
-        energy = np.linalg.norm([px_val, py_val, pz_val])
-        four_mmtm = np.array([energy, px_val, py_val, pz_val])
-        total_four_momenta[int(jet_id)] += four_mmtm
+        ene_val = np.linalg.norm([px_val, py_val, pz_val])
 
-        jet_ene[int(jet_id)] += energy
+        jet_ene[int(jet_id)] += ene_val
         jet_px[int(jet_id)] += px_val
         jet_py[int(jet_id)] += py_val
         jet_pz[int(jet_id)] += pz_val

src/main_one_time.py
@@ -40,7 +40,7 @@ eta = pseudorapidity(p_mag, pz)
 p_T = np.sqrt(px**2 + py**2)
 # Stats for jets
 jet_enes, jet_pxs, jet_pys, jet_pzs = calculate_four_momentum_massless(jet_ids, px, py, pz)
-jet_p2s = contraction(jet_enes, jet_pxs, jet_pys, jet_pzs)
+jet_p2s = contraction2(jet_enes, jet_pxs, jet_pys, jet_pzs)
 jet_masses = np.sqrt(jet_p2s)
 
 # Kinda fun to print