8289a18f854ba7523eb2ab55d25df9a83be8421d
Roy Siu <roysoup@outlook.com>
2024-12-04 22:40:53 +0000

    Minor tidy

--- DDPMLHC/generate_plots/generate_1d_plots.py
+++ DDPMLHC/generate_plots/generate_1d_plots.py
@@ -4,6 +4,7 @@
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.ticker import LogLocator
 import seaborn as sb
 import multiprocessing
 
@@ -58,7 +59,7 @@ class Set2DP(mpl.ticker.ScalarFormatter):
    def _set_format(self):
       self.format = "%.2f"
 
-def plot_1D_hist(name, data, xlog=False, plot_params=DEFAULT_PLOT_PARAMS, save_path=save_path, save_filename="out", x_min=None, x_max=None, ax=None, plot_ylabel=True, manual_xticklabel =  False, xtick_labels = None):
+def plot_1D_hist(name, data, xlog=False, plot_params=DEFAULT_PLOT_PARAMS, save_path=save_path, save_filename="out", x_min=None, x_max=None, ax=None, plot_ylabel=True):
     """Plot a 1D histogram. Optionally plot on a provided axis."""
     if ax is None:
         fig, ax = plt.subplots(figsize=(10, 6))  # Create a new figure if no axes provided
@@ -66,7 +67,7 @@ def plot_1D_hist(name, data, xlog=False, plot_params=DEFAULT_PLOT_PARAMS, save_p
         fig = None  # If ax provided, do not create a new figure
     # Copy default params if not already assigned
     plot_params.update({ k:v for k, v in DEFAULT_PLOT_PARAMS.items() if k not in plot_params})
-    sb.histplot(data, ax=ax,log_scale=(xlog, False), **plot_params)
+    sb.histplot(data, ax=ax, log_scale=(xlog, False), **plot_params)
 
     ax.set_xlabel(name, fontsize=16)
     ax.set_xlim(left=x_min, right=x_max)
@@ -79,15 +80,6 @@ def plot_1D_hist(name, data, xlog=False, plot_params=DEFAULT_PLOT_PARAMS, save_p
     
     if save_path and fig:
         fig.savefig(f"{save_path}/{save_filename}.png", dpi=600)
-    if manual_xticklabel and xtick_labels is not None:
-        ax.set_xticklabels(xtick_labels)
-    if name == "Jet Transverse Momentum $p_T$ [GeV]":
-        # Manually set labels to fit properly for the subplots
-        labels = [item.get_text() for item in ax.get_xticklabels()]
-        # ax.xaxis.set_major_locator(mpl.ticker.LogLocator(base=10.0, subs=None))
-        minor_locator = mpl.ticker.LogLocator(base=10.0, subs=[3, 6, 10])
-        ax.xaxis.set_minor_locator(minor_locator)
-        # ax.set_xticklabels(labels)
 
     return fig, ax
 
@@ -101,7 +93,7 @@ hist_data = [
 
     ("Jet Mass [GeV]", jet_mass, False, {}, save_path, "jet_mass", None, 250),
     ("Jet Pseudorapidity $\eta$", jet_eta, False, {"bins": 50}, save_path, "jet_eta"),
-    ("Jet Transverse Momentum $p_T$ [GeV]", jet_pT, True, {"bins": 50}, save_path, "jet_pT")
+    ("Jet Transverse Momentum $p_T$ [GeV]", jet_pT, True, {"bins": 50}, save_path, "jet_pT", None, 1000)
 ]
 
 # plot_1D_hist("Momentum $p$ [GeV]", p, save_filename="p", xlog=True)
@@ -120,16 +112,14 @@ pool.join()
 
 # Plot multiplot
 
-
-    
 hist_data = [
     ("Momentum $p$ [GeV]", p, {"xlog": True}),
     ("Pseudorapidity $\eta$", eta, {}),
     ("Transverse Momentum $p_T$ [GeV]", p_T, {"xlog": True}),
 
     ("Jet Mass [GeV]", jet_mass, {"x_max": 250}),
-    ("Jet Pseudorapidity $\eta$", jet_eta, {"bins": 50}),
-    ("Jet Transverse Momentum $p_T$ [GeV]", jet_pT, {"xlog": True, "bins": 50},)
+    ("Jet Pseudorapidity $\eta$", jet_eta, {"bins": 30}),
+    ("Jet Transverse Momentum $p_T$ [GeV]", jet_pT, {"xlog": True, "bins": 50, "x_max": 1000},)
 ]
 print("Plotting combined histogram...")
 
