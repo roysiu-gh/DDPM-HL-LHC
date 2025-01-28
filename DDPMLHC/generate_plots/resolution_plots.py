f5b082060f32cce613d9faf6d069f686ebb766af
Chung <dev.chpxu@outlook.com>
2025-01-28 17:48:51 +0000

    Updated Mean energy difference

--- DDPMLHC/generate_plots/resolution_plots.py
+++ DDPMLHC/generate_plots/resolution_plots.py
@@ -1,26 +1,34 @@
 # from visualisation import plot_detections, count_hist, energy_hist, generate_2dhist
 from DDPMLHC.config import *
-from DDPMLHC.dataset_ops.data_loading import select_event_deprecated
 from DDPMLHC.calculate_quantities import *
+from DDPMLHC.dataset_ops.data_loading import *
+from DDPMLHC.dataset_ops.process_data import *
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import time
 import multiprocessing
-start_time_global = time.time()
+# start_time_global = time.time()
 
 mpl.rcParams.update(MPL_GLOBAL_PARAMS)
 
-pile_up = np.genfromtxt(
-    PILEUP_PATH, delimiter=",", encoding="utf-8", skip_header=1, max_rows=MAX_DATA_ROWS
-)
-tt = np.genfromtxt(
-    TT_PATH, delimiter=",", encoding="utf-8", skip_header=1, max_rows=MAX_DATA_ROWS
-)
-tt_int = np.genfromtxt(
-    TT_INTER_PATH, delimiter=",", encoding="utf-8", skip_header=1, max_rows=MAX_DATA_ROWS
-)
-def quantity_diff(jet_ids, jet_px, jet_py, jet_pz, jet_mass, jet_pt, pu_px, pu_py, pu_pz):
+# pile_up = np.genfromtxt(
+#     PILEUP_PATH, delimiter=",", encoding="utf-8", skip_header=1, max_rows=MAX_DATA_ROWS
+# )
+# tt = np.genfromtxt(
+#     TT_PATH, delimiter=",", encoding="utf-8", skip_header=1, max_rows=MAX_DATA_ROWS
+# )
+# tt = EventSelector(tt)
+# pile_up = EventSelector(pile_up)
+
+# mus = np.arange(0,51,step=1)
+# high_PU_no = pile_up.max_ID
+
+
+# tt_int = np.genfromtxt(
+#     TT_INTER_PATH, delimiter=",", encoding="utf-8", skip_header=1, max_rows=MAX_DATA_ROWS
+# )
+def quantity_diff(jet_ids, jet_px, jet_py, jet_pz, pu_px, pu_py, pu_pz):
     """
     This function calculates and returns q_\mu^jet - q_0^jet for different jets with \mu pileups versus no pile_up.
 
@@ -40,9 +48,10 @@ def quantity_diff(jet_ids, jet_px, jet_py, jet_pz, jet_mass, jet_pt, pu_px, pu_p
     """
     # TODO
     jet_4mmtm = calculate_four_momentum_massless(jet_ids, jet_px, jet_py, jet_pz)
-    summed_jet_4mmtm = np.sum(jet_4mmtm, axis = 1)
+    summed_jet_4mmtm = jet_4mmtm
+    # summed_jet_4mmtm = np.sum(jet_4mmtm, axis = 1)
     # j_m = contraction(summed_jet_4mmtm[0], summed_jet_4mmtm[1], summed_jet_4mmtm[2], summed_jet_4mmtm[3])
-    jet_energy = summed_jet_4mmtm[0]
+    jet_energy = np.sum(summed_jet_4mmtm[0])
     # jet_pt = np.sqrt(summed_jet_4mmtm[1] +  summed_jet_4mmtm[2])
     # jet_energy = np.sum(jet_four_momenta[0]) # p^\nu = (E,px,py,pz) in natural units
     # print("jet_energy:", jet_energy)
@@ -69,7 +78,7 @@ def quantity_diff(jet_ids, jet_px, jet_py, jet_pz, jet_mass, jet_pt, pu_px, pu_p
     # print("c_p2", c_p2)
     c_m = np.sqrt(c_p2)
     # mass difference, energy difference, p_T difference, eta difference, phi difference
-    differences = (c_m - jet_mass, total_energy - jet_energy, c_pt - jet_pt)
+    differences = total_energy - jet_energy
     # print("differences", differences)
     return differences
  # p^\nu = (E,px,py,pz) in natural units
@@ -82,15 +91,11 @@ def quantity_diff(jet_ids, jet_px, jet_py, jet_pz, jet_mass, jet_pt, pu_px, pu_p
     # elif q == "eta":
 
     # elif q == "phi"
-pile_up_indices = pile_up[:,0]
-max_event_num = np.unique(pile_up_indices).astype(int)
-jet_masses = tt_int[:,6]
-jet_pt = tt_int[:,7]
-y_qlabel = {
-    "mass": r'$\braket{m_{\mu}^{\text{jet}} - m_{0}^{\text{jet}}}$ [GeV]',
-    "energy": r"$\braket{E_{\mu}^{\text{jet}} - E_{0}^{\text{jet}}}$ [GeV]",
-    "pt": r"$\braket{p_{T,\mu}^{\text{jet}} - p_{T,0}^{\text{jet}}}$ [GeV]"
-}
+# pile_up_indices = pile_up[:,0]
+# max_event_num = np.unique(pile_up_indices).astype(int)
+# jet_masses = tt_int[:,6]
+# jet_pt = tt_int[:,7]
+
 def mean_quantity_diff(params):
     """
     For a given number of pile_up events mu in MUs
@@ -105,78 +110,52 @@ def mean_quantity_diff(params):
     """
     # max_jet_no = np.shape(np.unique(jet_data[:,0]))[0]
     # Store jet COMS
-    jet_data, pile_up_data, mu, max_event_num, max_jet_no, jet_masses, jet_pt = params
+    jet_data, pile_up_data, mu, max_jet_no, high_PU_no= params
     # for ind,mu in enumerate(MUs):
     print("Begin mu = ", mu)
     m_total = 0
     E_total = 0
     p_T_total = 0
-    start_time = time.time()
+    # start_time = time.time()
     # print("ind", ind)
     # sys.exit(1)
     
-    pu_nos = np.random.randint(low = 0, high = high_PU_no, size = mu, dtype=np.int32)
+    # pu_nos = pile_up_data.select_event()
     # event_IDS = np.random.choice(max_event_num, size = (max_jet_no, mu))
     # valid_event_IDS = event_IDS[np.isin(event_IDS, max_event_num)]        print(event_IDS)
     for jet_no in range(0, max_jet_no):
-        selected_events = event_IDS[jet_no]
+        cd = jet_data.select_event(jet_no)
+        # print(f"jet {jet_no}: ",cd)
+        if mu != 0:
+            event_IDS = np.random.randint(low = 0, high = high_PU_no, size = mu, dtype=np.int32)
+            # print(event_IDS)
+            pile_ups = [pile_up_data.select_event(event_ID) for event_ID in event_IDS]
+            # print("Pile ups: ", pile_ups)
+            pile_ups = np.vstack(pile_ups)
+            pile_ups = np.delete(pile_ups, [0,1,2], axis=1)
+            E= quantity_diff(cd[:,0], cd[:,3],cd[:,4],cd[:,5], pile_ups[:,0], pile_ups[:,1], pile_ups[:,2])
+            # print(E)
+        else:
+            # 0 pile  up means diff is 0 anyways
+            E = 0
         # print(f"Jet_No: {jet_no}, event IDs: {event_IDS}")
         # selected_pile_ups = [select_event(pile_up_data, event_ID, filter=False) for event_ID in event_IDS]
         # selected_pile_ups = np.vstack(selected_pile_ups)
         # exit(1)
-        selected_pile_ups = pile_up_data[np.isin(pile_up_indices, selected_events)]
-        cd = select_event_deprecated(jet_data, jet_no)
-        m, E, p_T = quantity_diff(cd[:,0], cd[:,3],cd[:,4],cd[:,5], jet_masses[jet_no], jet_pt[jet_no], selected_pile_ups[:,3], selected_pile_ups[:,4], selected_pile_ups[:,5])
-        m_total += m
+        # selected_pile_ups = pile_up_data[np.isin(pile_up_indices, selected_events)]
+       
+        # m_total += m
         E_total += E
-        p_T_total += p_T
+        # p_T_total += p_T
     # Mean over number of jets
-    end_time = time.time()
-    print(f"Loop mu = {mu}: {end_time - start_time} seconds")
-    m_total /= max_jet_no
+    # end_time = time.time()
+    # print(f"Loop mu = {mu}: {end_time - start_time} seconds")
+    # m_total /= max_jet_no
     E_total /= max_jet_no
-    p_T_total /= max_jet_no
+    # p_T_total /= max_jet_no
     # print(m_total)
     # print(E_total)
     # print(p_T_total)
     print(f"End mu = {mu}")
-    return m_total, E_total, p_T_total
+    return E_total
 
-# Array of jet etas and phis
-# jet_centres = tt_int[:,4:6]
-# tt_masked = [delta_R(jet_centre, tt)]
-rlim = 50
-MUs = np.linspace(0,rlim,rlim+1, dtype=np.int64)
-max_jet_no = 1000
-data_y = np.zeros((3, len(MUs)))
-
-tasks = [
-        (tt, pile_up, mu, pile_up_indices, max_jet_no, jet_masses, jet_pt) 
-        for mu in MUs
-    ]
-
-with multiprocessing.Pool() as pool:
-    results = pool.map(mean_quantity_diff, tasks)
-pool.close()
-pool.join()
-
-for ind, (m_total, E_total, p_T_total) in enumerate(results):
-    data_y[0][ind] += m_total
-    data_y[1][ind] += E_total
-    data_y[2][ind] += p_T_total
-
-for name,data in zip(["Mass", "Energy", "pT"], data_y):
-    fig  = plt.figure(figsize=(8,6))
-    plt.tight_layout()
-    plt.plot(MUs, data)
-    plt.xlabel("$\mu$")
-    plt.ylabel(y_qlabel[name.lower()])
-    plt.xlim(0, np.max(MUs))
-    plt.ylim(0 if 0 < np.min(data) else np.min(data), np.max(data))
-    plt.savefig(f"{CWD}/data/plots/Mean_{name}_diff.pdf", format="pdf")
-    plt.close()
-
-end_time_global = time.time()
-print(f"Global runtime: {end_time_global - start_time_global} seconds")
-
-# mean_quantity_diff(tt, pile_up, mus, max_jet_no=1000)
--- DDPMLHC/main_cx.py
+++ DDPMLHC/main_cx.py
@@ -1,15 +1,16 @@
 # Package imports
 import numpy as np
 import matplotlib as mpl
+import sys
 # Local imports
 from DDPMLHC.config import *
 from DDPMLHC.calculate_quantities import *
 from DDPMLHC.dataset_ops.process_data import *
 from DDPMLHC.generate_plots.generate_1d_plots import plot_1d_histograms
-
+from DDPMLHC.generate_plots.resolution_plots import *
+import multiprocessing
 mpl.rcParams.update(MPL_GLOBAL_PARAMS)
-
-MAX_DATA_ROWS = 100_000
+MAX_DATA_ROWS = None
 
 # === Read in data
 print("0 :: Loading original data")
@@ -17,74 +18,63 @@ pile_up = np.genfromtxt(
     PILEUP_PATH, delimiter=",", encoding="utf-8", skip_header=1, max_rows=MAX_DATA_ROWS
 )
 tt = np.genfromtxt(
-    TT_PATH, delimiter=",", encoding="utf-8", skip_header=1, max_rows=10*MAX_DATA_ROWS
+    TT_PATH, delimiter=",", encoding="utf-8", skip_header=1, max_rows=MAX_DATA_ROWS
 )
 print("FINISHED loading data\n")
 
 #################################################################################
+tt = EventSelector(tt)
+pile_up = EventSelector(pile_up)
 
-# === Create noisy events
-print("1 :: Creating noisy events")
-make_noisy_data(range(TTBAR_NUM), tt, pile_up, 0, save_path=INTERMEDIATE_PATH)
-make_noisy_data(range(TTBAR_NUM), tt, pile_up, 1, save_path=INTERMEDIATE_PATH)
-make_noisy_data(range(TTBAR_NUM), tt, pile_up, 3, save_path=INTERMEDIATE_PATH)
-make_noisy_data(range(TTBAR_NUM), tt, pile_up, 5, save_path=INTERMEDIATE_PATH)
-make_noisy_data(range(TTBAR_NUM), tt, pile_up, 10, save_path=INTERMEDIATE_PATH)
-print("FINISHED creating noisy events\n")
+mus = np.arange(0,5,step=1)
+high_PU_no = int(pile_up.max_ID)
+max_jet_no = int(tt.max_ID)
+print("high pu", high_PU_no)
+print("max jet", max_jet_no)
+data_y = np.zeros((3, len(mus)))
 
-# === Collapse noisy data to event-level
-print("2 :: Collapsing noisy data to event-level")
-calculate_event_level_quantities(0, INTERMEDIATE_PATH)
-calculate_event_level_quantities(1, INTERMEDIATE_PATH)
-calculate_event_level_quantities(3, INTERMEDIATE_PATH)
-calculate_event_level_quantities(5, INTERMEDIATE_PATH)
-calculate_event_level_quantities(10, INTERMEDIATE_PATH)
-print("FINISHED collapsing noisy data to event-level\n")
+tasks = [
+        (tt, pile_up, mu, max_jet_no, high_PU_no+1) 
+        for mu in mus
+    ]
+energy_data = np.zeros(len(tasks))
+for indx,task in enumerate(tasks):
+    energy_data[indx] += mean_quantity_diff(task)
+# with multiprocessing.Pool(processes=int(len(mus) / 4)) as pool:
+#     results = pool.map(mean_quantity_diff, tasks)
+# pool.close()
+# pool.join()
 
-# === Draw 1D histograms
-print("3 :: Drawing 1D histograms")
-plot_1d_histograms(mu=0)
-plot_1d_histograms(mu=1)
-plot_1d_histograms(mu=3)
-plot_1d_histograms(mu=5)
-plot_1d_histograms(mu=10)
-print("FINISHED drawing 1D histograms\n")
+# for ind,E_total in enumerate(results):
+    # data_y[0][ind] += m_total
+    
+    # data_y[2][ind] += p_T_total
 
-# print("4 :: Drawing overlaid histograms with varying mu")
-# print("Loading intermediate data...")
-# tt = np.genfromtxt(
-#     TT_EXT_PATH, delimiter=",", encoding="utf-8", skip_header=1, max_rows=MAX_DATA_ROWS
-# )
-# px = tt[:, 1]
-# py = tt[:, 2]
-# pz = tt[:, 3]
-# eta = tt[:, 4]
-# p_T = tt[:, 6]
-# p = p_magnitude(px, py, pz)
-# OUT_PATH_1D_HIST = f"{CWD}/data/plots/1D_histograms/particles/"
-# hist_data_particles = [
-#     {
-#         "name": "Momentum $p$ [GeV]",
-#         "data": p,
-#         "plot_params": {"xlog": True},
-#         "save_filename": "p",
-#         "save_path": OUT_PATH_1D_HIST,
-#     },
-#     {
-#         "name": "Pseudorapidity $\eta$",
-#         "data": eta,
-#         "plot_params": {},
-#         "save_filename": "eta",
-#         "save_path": OUT_PATH_1D_HIST,
-#     },
-#     {
-#         "name": "Transverse Momentum $p_T$ [GeV]",
-#         "data": p_T,
-#         "plot_params": {"xlog": True},
-#         "save_filename": "pT",
-#         "save_path": OUT_PATH_1D_HIST,
-#     },
-# ]
-# print("FINISHED drawing overlaid histograms\n")
+# for name,data in zip(["Mass", "Energy", "pT"], data_y):
+#     fig  = plt.figure(figsize=(8,6))
+#     plt.tight_layout()
+#     plt.plot(MUs, data)
+#     plt.xlabel("$\mu$")
+#     plt.ylabel(y_qlabel[name.lower()])
+#     plt.xlim(0, np.max(MUs))
+#     plt.ylim(0 if 0 < np.min(data) else np.min(data), np.max(data))
+#     plt.savefig(f"{CWD}/data/plots/Mean_{name}_diff.pdf", format="pdf")
+#     plt.close()
+y_qlabel = {
+    "mass": r'$\braket{m_{\mu}^{\text{jet}} - m_{0}^{\text{jet}}}$ [GeV]',
+    "energy": r"$\braket{E_{\mu}^{\text{jet}} - E_{0}^{\text{jet}}}$ [GeV]",
+    "pt": r"$\braket{p_{T,\mu}^{\text{jet}} - p_{T,0}^{\text{jet}}}$ [GeV]"
+}
+fig  = plt.figure(figsize=(8,6))
+plt.tight_layout()
+plt.plot(mus, energy_data)
+plt.xlabel("$\mu$")
+plt.ylabel(r"$\braket{E_{\mu}^{\text{jet}} - E_{0}^{\text{jet}}}$ [GeV]")
+plt.xlim(0, np.max(mus))
+plt.ylim(0 if 0 < np.min(energy_data) else np.min(energy_data), np.max(energy_data))
+plt.savefig(f"{CWD}/data/plots/Mean_Energy_diff.pdf", format="pdf")
+plt.close()
+# end_time_global = time.time()
+# print(f"Global runtime: {end_time_global - start_time_global} seconds")
 
 print("DONE ALL.")
