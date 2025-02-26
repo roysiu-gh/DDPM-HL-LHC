e20a9de975c77e91ad977b07adde619498f07dbc
Roy Siu <roysoup@outlook.com>
2025-02-26 16:25:14 +0000

    Move some files and put gridgen into func.

--- DDPMLHC/main_rs.py
+++ DDPMLHC/main_rs.py
@@ -71,54 +71,46 @@ print("FINISHED loading data\n")
 
 #################################################################################
 
-mu = 0
-output_path = f"{CWD}/data/3-grid/"
-output_filename = f"noisy_mu{mu}_event_level_from_grid{BMAP_SQUARE_SIDE_LENGTH}.csv"
-output_filepath = f"{output_path}/{output_filename}"
+def gen3grid(mu):
+    output_path = f"{CWD}/data/3-grid/mu{mu}/"
+    output_filename = f"noisy_mu{mu}_event_level_from_grid{BMAP_SQUARE_SIDE_LENGTH}.csv"
+    output_filepath = f"{output_path}/{output_filename}"
 
-generator = NoisyGenerator(tt, pile_up, mu=mu)
-combined = []
+    generator = NoisyGenerator(tt, pile_up, mu=mu)
+    combined = []
 
-for idx, _ in enumerate(generator):
-    grid = generator.get_grid(normalise=False)
-    
-    enes, detas, dphis = grid_to_ene_deta_dphi(grid, N=generator.bins)
-    pxs, pys, pzs = deta_dphi_to_momenta(enes, detas, dphis)
-    event_quantities = particle_momenta_to_event_level(enes, pxs, pys, pzs)
-    event_mass, event_px, event_py, event_pz, event_eta, event_phi, event_pT = event_quantities
+    for idx, _ in enumerate(generator):
+        grid = generator.get_grid(normalise=False)
+        
+        enes, detas, dphis = grid_to_ene_deta_dphi(grid, N=generator.bins)
+        pxs, pys, pzs = deta_dphi_to_momenta(enes, detas, dphis)
+        event_quantities = particle_momenta_to_event_level(enes, pxs, pys, pzs)
+        event_mass, event_px, event_py, event_pz, event_eta, event_phi, event_pT = event_quantities
 
-    event_level = np.array([
-        idx,
-        event_px,
-        event_py,
-        event_pz,
-        event_eta,
-        event_phi,
-        event_mass,
-        event_pT,
-    ])
+        event_level = np.array([
+            idx,
+            event_px,
+            event_py,
+            event_pz,
+            event_eta,
+            event_phi,
+            event_mass,
+            event_pT,
+        ])
 
-    combined.append(np.copy(event_level))
+        combined.append(np.copy(event_level))
 
-all_data = np.vstack(combined)
+    all_data = np.vstack(combined)
 
-# Final check before saving
-if np.any(np.isnan(all_data)):
-    print("\nWarning: NaN values in final data:")
-    print(f"Total NaN count: {np.sum(np.isnan(all_data))}")
-    print("NaN locations (row, column):")
-    nan_rows, nan_cols = np.where(np.isnan(all_data))
-    column_names = ['event_id', 'px', 'py', 'pz', 'eta', 'phi', 'mass', 'p_T']
-    for row, col in zip(nan_rows, nan_cols):
-        print(f"Row {row}, Column {column_names[col]}")
+    np.savetxt(
+        output_filepath,
+        all_data,
+        delimiter=",",
+        header="event_id,px,py,pz,eta,phi,mass,p_T",
+        comments="",
+        fmt="%i,%10.10f,%10.10f,%10.10f,%10.10f,%10.10f,%10.10f,%10.10f"
+    )
+    plot_1d_histograms(mu, event_stats_path=output_filepath, output_path=f"{output_path}/grid{BMAP_SQUARE_SIDE_LENGTH}")
 
-np.savetxt(
-    output_filepath,
-    all_data,
-    delimiter=",",
-    header="event_id,px,py,pz,eta,phi,mass,p_T",
-    comments="",
-    fmt="%i,%10.10f,%10.10f,%10.10f,%10.10f,%10.10f,%10.10f,%10.10f"
-)
-
-plot_1d_histograms(mu, event_stats_path=output_filepath, output_path=f"{output_path}/grid{BMAP_SQUARE_SIDE_LENGTH}")
+BMAP_SQUARE_SIDE_LENGTH = 16
+gen3grid(mu=0)
